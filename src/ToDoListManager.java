adef432bb9d9fd5ad703955a96ee40cd824807cf
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2023-05-15 14:48:47 +0100

    Updated the todo

--- src/ToDoListManager.java
+++ src/ToDoListManager.java
@@ -1,113 +1,118 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ToDoListManager{
     private List<String> tasks;
 
     public ToDoListManager(){
-        tasks = new ArrayList<String>();
+        tasks = new ArrayList<>();
     }
-    public void addTask(String task){
-        tasks.add(task);
+
+    public void addTodo(String task){
+         tasks.add(task);
     }
-    public void removeTask(int index){
-        if(index <= 0 && index < tasks.size()){
+    public void removeTodo(int index){
+        if(index < 0 && index < tasks.size()){
             tasks.remove(index);
         }
+
     }
     public void markTaskAsCompleted(int index){
-        if(index >= 0 && index < tasks.size()){
+        if(index >= 0 && index < tasks.size()) {
             String task = tasks.get(index);
-            tasks.set(index,"[Completed] " +  task);
+            tasks.set(index,"[Completed]" + task);
         }
-    }
-    public void displayTask(){
-        System.out.println("Todo task:");
-        for(int i=0; i < tasks.size(); i++){
-            System.out.println(i + ".  "+tasks.get(i));
+        }
+
+        public void displayTask(){
+        System.out.println("Todo list:");
+        for(int i = 0; i < tasks.size(); i++){
+            System.out.println(i +", "+ tasks.get(i));
         }
         System.out.println();
-    }
-
-    public void saveTasksToFile(String filename){
-        try(PrintWriter writer = new PrintWriter(new FileWriter(filename))) {
-            for(String task: tasks){
-                writer.println(task);
-            }
-            System.out.println("To-Do list saved to " + filename);
-        } catch(IOException e){
-            System.out.println("Error saving To-Do "+ e.getMessage());
         }
-}
-public void loadTaskFromFile(String filename){
+        public void saveTaskFromFile(String filename){
+            try(PrintWriter writer = new PrintWriter(new FileWriter(filename))){
+                for (String task : tasks){
+                    writer.println(task);
+                }
+                System.out.println("File saved Successfully "+ filename);
+            }
+            catch(IOException e){
+                System.out.println("Could not save File"+ e.getMessage());
+            }
+        }
+        public void loadTaskFromFile(String filename){
         tasks.clear();
-        try(Scanner scanner = new Scanner(new File(filename))){
-            while(scanner.hasNextLine()){
-                String task = scanner.nextLine();
+            try(Scanner scanner = new Scanner(new File(filename))){
+                while (scanner.hasNextLine()) {
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                }
+                System.out.println("Loaded file successfully "+ filename);
             }
-            System.out.println("To DO list loaded from " + filename);
+            catch(IOException e){
+                    System.out.println("Error loading file "+ e.getMessage());
+            }
         }
-        catch(IOException e){
-            System.out.println("Error loading To-DO "+ e.getMessage());
-        }
-}
-
-public static void main(String[] args){
+        public static void main(String[] args){
         ToDoListManager manager = new ToDoListManager();
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner  = new Scanner(System.in);
         boolean running = true;
 
-        while (running){
-                System.out.print("1. Add Task \b ");
-                System.out.print("2. Remove Task \b ");
-                System.out.print("3. Mark Task as Completed \b ");
-                System.out.print("4. View Task \b ");
-                System.out.print("5. Save Task to file  \b ");
-                System.out.print("6. Load Task from file \b ");
-                System.out.print("7. Quit \b ");
-                System.out.print("Enter you choice: \b ");
-                int  choice = scanner.nextInt();
-                scanner.nextLine();
+        while(running){
+            System.out.println("1. Add Task");
+            System.out.println("2. Remove Task");
+            System.out.println("3. Mark Completed Task");
+            System.out.println("4. Display Task");
+            System.out.println("5. Save Task");
+            System.out.println("6. Load Task");
+            System.out.println("7. Quit");
+            System.out.println("Enter your choice");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-                switch (choice){
-                    case 1:
-                        System.out.println("Add task: \b ");
-                        String task  = scanner.nextLine();
-                        manager.addTask(task);
-                        break;
-                    case 2:
-                        System.out.println("Remove task: \b ");
-                        int index = scanner.nextInt();
-                        manager.removeTask(index);
-                        break;
-                    case 3:
-                        System.out.println("Mark Completed Task: \b ");
-                        int completedTask = scanner.nextInt();
-                        manager.markTaskAsCompleted(completedTask);
-                        break;
-                    case 4:
-                        manager.displayTask();
-                        break;
-                    case 5:
-                        System.out.println("Enter Filename to save: \b ");
-                        String saveFileName = scanner.nextLine();
-                        manager.saveTasksToFile(saveFileName);
-                        break;
-                    case 6:
-                        System.out.println("Enter Filename to save: \b ");
-                        String loadFilename = scanner.nextLine();
-                        manager.loadTaskFromFile(loadFilename);
-                        break;
-                    case 7:
-                        running = false;
-                        break;
-                    default:
-                        System.out.println("Invalid choice,Try again \b ");
-                }
+            switch (choice){
+                case 1:
+                    System.out.println("Enter Task");
+                    String task = scanner.nextLine();
+                    manager.addTodo(task);
+                    break;
+                case 2:
+                    System.out.println("Remove Task");
+                    int index = scanner.nextInt();
+                    manager.removeTodo(index);
+                    break;
+                case 3:
+                    System.out.println("Select Completed Task");
+                    int markSelected = scanner.nextInt();
+                    manager.markTaskAsCompleted(markSelected);
+                    break;
+                case 4:
+                    manager.displayTask();
+                    scanner.nextLine();
+                    break;
+                case 5:
+                    System.out.println("Enter file name to save");
+                    String saveFileName= scanner.nextLine();
+                    manager.saveTaskFromFile(saveFileName);
+                    break;
+                case 6:
+                    System.out.println("Enter File name to load");
+                    String loadFilename = scanner.nextLine();
+                    manager.loadTaskFromFile(loadFilename);
+                    break;
+                case 7:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice, pick again");
+            }
         }
         scanner.close();
-    System.out.println("Program Tarminated");
-}
+        System.out.println("Process Terminated");
+
+        }
 }
